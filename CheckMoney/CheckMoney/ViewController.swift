b3dee394a854ea714738ecf3a2f41ea8de7cea04
SeungYeon Kim <seungyeon.kim@devsisters.com>
2021-10-21 19:11:26 +0900

    Add new project

---
+++ CheckMoney/CheckMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckMoney
+//
+//  Created by SeungYeon Kim on 2021/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
